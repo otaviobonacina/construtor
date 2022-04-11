edc3a697b5d21c31ae202fdb0f1e363cc8a7f0d3
otaviobonacina <otavio.bernado@fatec.sp.gov.br>
2022-04-11 19:46:27 +0000

    Construtor

---
+++ MinhaCalculadora.java
@@ -0,0 +1,16 @@
+public class MinhaCalculadora{
+    public MinhaCalculadora(){
+ 
+    }
+    public int soma(int num1, int num2){
+        return num1 + num2;
+    }
+    public int soma(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+    public double soma(double num1,double num2){
+        return num1 + num2;
+    }
+ }
+ // MinhaCalculadora calc=new MinhaCalculadora();
+ //System.out.println(calc.soma(38,1));
--- Principal.java
+++ Principal.java
@@ -5,6 +5,14 @@ public class Principal {
         Carro car=new Carro();
         Carro car2= new Carro ("1.0", "Up Tsi", 4, 63000);
         car2.dadosVeiculo();
+        MinhaCalculadora calc=new MinhaCalculadora();
+        System.out.println(calc.soma(38,1));
+        MinhaCalculadora calc2=new MinhaCalculadora();
+        System.out.println(calc.soma(38,1,1));
+        MinhaCalculadora calc3=new MinhaCalculadora();
+        System.out.println(calc.soma(3.50,2));
+        
+    }
     }
     
-}
+
