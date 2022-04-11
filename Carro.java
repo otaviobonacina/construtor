92e71da2dd268a6c9a5db830a4d10435a703dda3
otaviobonacina <otavio.bernado@fatec.sp.gov.br>
2022-04-11 18:26:51 +0000

    Construtor

---
+++ Carro.java
@@ -0,0 +1,14 @@
+public class Carro{
+    private String motor;
+    private String modelo;
+    private int roda;
+    private double preço;
+
+    public Carro() {
+
+    }
+    public Carro(String motor,String modelo, int roda, double preço){
+
+    }
+    // faça um construtor tipo trator, tração
+}
---
+++ Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void aim(String[]args){
+        Bicicleta bike= new Bicicleta();
+
+        Carro car=new Carro();
+        Carro car= new Carro ("1.0", "Up", 4, 63000);
+    }
+    
+}
